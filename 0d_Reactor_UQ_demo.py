481df0723181277ce7ad40cd00307e16d2371772
Dwyer Deighan <dwyerdei@buffalo.com>
2023-04-11 00:28:49 -0400

    commiting new version of 0d_Reactor_UQ_demo.py demo which seems to be wokring?

--- 0d_Reactor_UQ_demo.py
+++ 0d_Reactor_UQ_demo.py
@@ -2,7 +2,7 @@ from ChemtabUQ import *
 
 sample_from_uncertainty=False
 
-df_fn = f'{os.environ["HOME"]}/data/chrest_contiguous_group_sample.csv'
+df_fn = f'{os.environ["HOME"]}/data/chrest_contiguous_group_sample100k.csv'
 moments_dataset = UQMomentsDataset(df_fn, inputs_like='Yi', outputs_like='souspec', group_key='group')
 
 # get ICs
@@ -10,8 +10,14 @@ moments_dataset = UQMomentsDataset(df_fn, inputs_like='Yi', outputs_like='souspe
 Yi_state=mu + th.randn(*sigma.shape)*sigma*sample_from_uncertainty
 Yi_sigma=sigma
 
-mean_regressor = th.load('mean_regressor.pt')
-std_regressor = th.load('std_regressor.pt') # technically this one was trained on MAE, close enough
+n_Yi = mu.shape[0]
+assert n_Yi == 53
+
+mean_regressor = UQModel.load_from_checkpoint('mean_regressor.ckpt', input_size=n_Yi)
+std_regressor = UQModel.load_from_checkpoint('std_regressor.ckpt', input_size=n_Yi*2, output_size=n_Yi)
+
+mean_regressor.eval()
+std_regressor.eval()
 
 n_time_steps = 100
 dt = 1e-7
@@ -21,7 +27,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 for i in range(n_time_steps):
-	Yi_dot = mean_regressor(Yi_state)
+	Yi_dot = mean_regressor(Yi_state.reshape(1,-1)).squeeze().detach()
 	Yi_state += Yi_dot*dt
 
 	Yi_state_pd = pd.Series(Yi_state.numpy(), index=moments_dataset.input_col_names)
