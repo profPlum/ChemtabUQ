7617405a750691c9d6ad8840e5dff692c3135ca2
Dwyer Deighan <dwyerdei@buffalo.com>
2023-09-01 19:34:35 -0400

    commiting chemtab_UQ-PCA.slurm with updates for new logging

--- chemtab_UQ_job_stub.sh
+++ chemtab_UQ_job_stub.sh
@@ -17,7 +17,9 @@ echo num nodes: $num_nodes
 [ -z "$EXTRA_PL_ARGS" ] && echo EXTRA_PL_ARGS is empty!! >&2
 
 # clear previous variables (NOT an input env variable)
-diagnostic_CLI_args="--trainer.logger True --trainer.profiler simple --model.device_stats_monitor True --trainer.track_grad_norm 2" # use these only for "debug mode"
+#diagnostic_CLI_args="--trainer.logger True --trainer.profiler simple --model.device_stats_monitor True --trainer.track_grad_norm 2" # use these only for "debug mode"
+diagnostic_CLI_args="--trainer.logger=pytorch_lightning.loggers.TensorBoardLogger --trainer.logger.save_dir=ChemtabUQ_TBlogs --trainer.logger.version=$SLURM_JOB_ID" #--trainer.logger.name=$SLURM_JOB_NAME"
+diagnostic_CLI_args="$diagnostic_CLI_args --trainer.profiler simple --model.device_stats_monitor True --trainer.track_grad_norm 2"
 EXTRA_PL_ARGS1="$EXTRA_PL_ARGS $PL_MAX_EPOCHS $diagnostic_CLI_args" # NOTE: EXTRA_PL_ARGS should be an environment variable given by the user when submitting sbatch!!
 lightning_CLI_args="$EXTRA_PL_ARGS1 --trainer.num_nodes=$num_nodes --trainer.devices=2 --trainer.accelerator=gpu --trainer.strategy=ddp --trainer.gradient_clip_algorithm=value --trainer.gradient_clip_val 0.25"
 # NOTE: gradient_clip_value=0.5 recommended by PL docs
@@ -33,12 +35,12 @@ conda activate pytorch_distributed_cuda3
 # IMPORTANT: pytorch_distributed_cuda3 is the "stable version" with pytorch-lightning==1.9.0 and regular CLI stuff working
 # pytorch_distributed_cuda is a possible "newer version" which is capable of using the A100 GPUs
 
-srun --ntasks-per-node=2 python ChemtabUQ.py fit --data.class_path=MeanRegressorDataModule $lightning_CLI_args 
+srun --ntasks-per-node=2 python ChemtabUQ.py fit --data.class_path=MeanRegressorDataModule $lightning_CLI_args --trainer.logger.name=$SLURM_JOB_NAME-Mu 
 mkdir mean_regressor
 mv model.* mean_regressor
 
 if ! [ $MEAN_ONLY ]; then
-	srun --ntasks-per-node=2 python ChemtabUQ.py fit --data.class_path=UQRegressorDataModule --data.mean_regressor_fn=mean_regressor/model.ckpt $lightning_CLI_args 
+	srun --ntasks-per-node=2 python ChemtabUQ.py fit --data.class_path=UQRegressorDataModule --data.mean_regressor_fn=mean_regressor/model.ckpt $lightning_CLI_args --trainer.logger.name=$SLURM_JOB_NAME-Sigma 
 	mkdir UQ_regressor
 	mv model.* UQ_regressor
 else
