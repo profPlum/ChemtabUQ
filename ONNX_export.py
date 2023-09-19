2117a8e76b3bd23f642e5464b0b99e2b90246cf1
Dwyer Deighan <dwyerdei@buffalo.com>
2023-09-19 15:35:26 -0400

    added rescaling layer & unit_L1_norm constraint wrapping to ONNX_export.py

--- ONNX_export.py
+++ ONNX_export.py
@@ -1,11 +1,67 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os, sys
+import ChemtabUQ
 import pytorch_lightning as pl
 import torch as pt
-import ChemtabUQ
-import os, sys
+import tensorflow.keras as keras
 import matplotlib
+
+######################### Post-Processing Layers #########################
+
+# Verified to work! 5/14/22 (in regards to relative tolerance, see the assert)
+# :return: rescaling_layer, (m,b) <-- includes params for manual scaling!
+def fit_rescaling_layer(scaler, inverse=True, layer_name='rescaling', n_samples=1000):
+    import sklearn, tensorflow as tf, tensorflow.keras as keras, numpy as np # metrics ... for sanity checks
+    import sklearn.linear_model
+    def R2(yt,yp): return tf.reduce_mean(1-tf.reduce_mean((yp-yt)**2, axis=0)/(tf.math.reduce_std(yt,axis=0)**2))
+    def rel_err(yt, yp): return tf.reduce_mean(tf.abs((yp-yt)/yt))
+
+    n_input_features = scaler.n_features_in_
+    data_scale = 100
+    dummy_input_data = (np.random.random(size=(n_samples,n_input_features))-0.5)*data_scale
+    inverted_data = scaler.inverse_transform(dummy_input_data) if inverse else scaler.transform(dummy_input_data)
+    def fit_rescaling_lms():
+        linear_models = []
+        for i in range(n_input_features):
+            lm = sklearn.linear_model.LinearRegression()
+            X_data = dummy_input_data[:,i].reshape(-1,1)
+            Y_data = inverted_data[:,i].reshape(-1,1)
+            lm.fit(X_data, Y_data)
+            assert lm.score(X_data, Y_data)==1.0 # assert R^2==1 (should be perfect fit)
+            linear_models.append(lm)
+        return linear_models
+    lms = fit_rescaling_lms()
+    m = np.array([lm.coef_ for lm in lms]).squeeze()
+    b = np.array([lm.intercept_ for lm in lms]).squeeze()
+    rescaling_layer = keras.layers.Rescaling(m, b, name=layer_name) # y=mx+b
+    rescale_inverted_data = rescaling_layer(dummy_input_data).numpy().astype('float64')
+    print('MAE/data_scale for inversion layer:', np.mean(np.abs(rescale_inverted_data-inverted_data))/data_scale)
+    print('R^2 for inversion layer:', R2(inverted_data, rescale_inverted_data).numpy())
+    print('Rel-error for inversion layer:', rel_err(inverted_data, rescale_inverted_data).numpy())
+    assert np.allclose(rescale_inverted_data, inverted_data)
+
+    return rescaling_layer, (m, b)
+
+#rescaling_layer, (m,b) = fit_rescaling_layer(dm.outputScaler)
+
+def add_unit_L1_layer_constraint(x, first_n_preserved=0, layer_name=None):
+    """ this is designed to constraint the [first_n_preserved:]
+    outputs from the inversion layer to fall between 0 & 1 and to sum to 1 """
+    from tensorflow.keras import layers
+
+    inputs_ = layers.Input((x.shape[1]-first_n_preserved,))
+    out = tf.math.maximum(inputs_, 0)
+    out = tf.math.minimum(out, 1)
+
+    out = out/tf.math.reduce_sum(out, axis=-1, keepdims=True)
+    model = keras.models.Model(inputs=inputs_, outputs=out, name='Unit_L1_constraint')
+    return layers.Concatenate(axis=-1, name=layer_name)([x[:,:first_n_preserved], model(x[:,first_n_preserved:])])
+
+##################################################################
+
+######################### ONNX/TF Export #########################
 
 def test_outputs(yt, yp):
     import tensorflow as tf
@@ -82,7 +138,7 @@ def convert_FFRegressor_to_TF(ckpt_path):
     print('done converting')
     return tf_rep, tf_path 
 
-from fit_rescaling_layer import fit_rescaling_layer, add_unit_L1_layer_constraint
+##################################################################
 
 # TODO: finish/debug me!!
 def export_CT_model_for_ablate(ckpt_path, add_hard_l1_constraint=False):
@@ -95,26 +151,39 @@ def export_CT_model_for_ablate(ckpt_path, add_hard_l1_constraint=False):
     moments_dataset = PLD_module.dataset.moments_dataset    
     
     output = input_ = keras.layers.Input(shape=keras_rep.input_shape[1:], name='input_1')
+    assert moments_dataset.input_scaler is None, 'input scaler export not supported yet, TODO: test inverse=False'
+    # TODO: test using fit_rescaling_layer(..., inverse=False) (should work for input scaler)
 
-    if moments_dataset.input_scaler:
-        print('fitting input rescaling layer!')
-        # IMPORTANT: you can't use fit_rescaling_layer for the input scaler b/c it is designed for output scaler only!
-        #input_scaling_layer, (m,b) = fit_rescaling_layer(moments_dataset.input_scaler, layer_name='input_rescaling')
-        output = moments_dataset.input_scaler.transform(output) #input_scaling_layer(input_)
     output = keras_rep(output)
     if moments_dataset.output_scaler:
         print('fitting input rescaling layer!')
-        #output_scaling_layer, (m,b) = fit_rescaling_layer(moments_dataset.output_scaler, layer_name='output_rescaling')
-        output=moments_dataset.output_scaler.inverse_transform(output) # does this work??
+        output_scaling_layer, (m,b) = fit_rescaling_layer(moments_dataset.output_scaler, 
+                                                          inverse=True, layer_name='output_rescaling')
+        output=output_scaling_layer(output)
+
+    if add_hard_l1_constraint:
+        output=add_unit_L1_layer_constraint(output, first_n_preserved=0)
 
     wrapper = keras.models.Model(inputs=input_, outputs=output) 
+    wrapper.save(model_path)
     return wrapper
 
-# this is the test case
+from glob import glob
+import random
+import os
+
+# default checkpoint is the test case
 if __name__=='__main__':
+    # get default checkpoint
+    candidate_ckpts=glob(f'./CT_logs_Mu/Selu-Scaled*/version_13521625/checkpoints/*.ckpt')
+    ckpt_path = random.choice(candidate_ckpts) 
+    print(ckpt_path)
     
-    ckpt_path='../CT_logs_Mu/Selu-Scaled-PCA-CT/version_13479785/checkpoints/epoch=5082-step=5083.ckpt'
-    if len(sys.argv)>1: ckpt_path=sys.argv[1]
-    keras_rep = convert_FFRegressor_to_TF(ckpt_path)
-    try: keras_rep.summary()
-    except: raise RuntimeError('keras representation is required for rescaling layer!') 
+    parser = argparse.ArgumentParser(description='exports CT models to TF with relevant post-processing layers (e.g. rescaling)')
+    parser.add_argument('checkpoint_path', type=str, default=ckpt_path, help='path to checkpoint to load, convert to TF & wrap') 
+    parser.add_argument('--hard_L1_unit_norm_constraint', action='store_true', help='turn on for the inverse model, it will enforce Yi constraints')
+    args = parser.parse_args() 
+    ckpt_path=args.checkpoint_path
+    
+    keras_rep = export_CT_model_for_ablate(ckpt_path)
+    #keras_rep = convert_FFRegressor_to_TF(ckpt_path)
