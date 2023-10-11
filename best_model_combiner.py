3e608aacdd26a18e6f23fddaab3d4af4f5ab6049
Dwyer Deighan <dwyerdei@buffalo.com>
2023-10-11 17:36:20 -0400

    added automation to find the best checkpoint within a given experiment for best_model_combiner.py makes usage much less tedious and error prone

--- best_model_combiner.py
+++ best_model_combiner.py
@@ -54,25 +54,54 @@ def make_aggregate_regressor(CPV_source_ckpt: str, souener_ckpt: str, inv_ckpt: 
     tf.keras.utils.plot_model(aggregate_regressor, to_file='aggregate_regressor.png', show_shapes=True, show_dtype=True)
     return aggregate_regressor
 
-# default checkpoint is the test case
-if __name__=='__main__':
-    # get default checkpoint
-    candidate_ckpts=glob(f'./CT_logs_Mu/*/version_*/checkpoints/*.ckpt')
-    print('all candidate checkpoints: ')
-    print(candidate_ckpts)
+# Verified to work 10/11/23
+def find_best_ckpt(search_dir):
+    ckpts = os.popen(f'find {search_dir} -name "*.ckpt"').read().strip()
+    
+    if ckpts: ckpts=ckpts.split('\n') # sanity checks...
+    else: raise FileNotFoundError('No checkpoints found in search path!')
+    assert all(['val_loss=' in ckpt_i for ckpt_i in ckpts]), 'Checkpoint naming format is incompatible! (must contain val_loss=*)'
 
-    parser = argparse.ArgumentParser(description='packages/aggregates V2 CT models for ablate')
-    parser.add_argument('--CPV_source_path', type=str, required=True, help='path to checkpoint of V2 CPV_source model')
-    parser.add_argument('--Souener_path', type=str, required=True, help='path to checkpoint of V2 Souener model')
-    parser.add_argument('--Inverse_path', type=str, required=True, help='path to checkpoint of V2 Inverse model')
+    import re
+    get_val_loss = lambda fn: float(re.sub(r"^.*val_loss=([0-9.]+).*$", r"\1", fn))
+    ckpts = sorted(ckpts, key=get_val_loss)
+    print('Sorted ckpts (top 5): \n'+'\n'.join(ckpts[:5])+'\n') 
+    return ckpts[0]
+
+if __name__=='__main__':
+    # show candidate checkpoint search paths
+    candidate_ckpts=glob(f'./CT_logs_Mu/*')
+    description='\n'+'='*100+'\n'+'='*100+'\n'
+    description+='ALL CANDIDATE CHECKPOINT SEARCH PATHS (aka experiment names): \n'
+    description+=', '.join(map(lambda x: x.replace('./CT_logs_Mu/', ''), candidate_ckpts))+'\n'
+    description+='='*100+'\n'+'='*100+'\n'
+    print(description)
+    import time; time.sleep(0.5)
+ 
+    #print('\n'+'='*100+'\n'+'='*100)
+    #print('All candidate checkpoint search paths (aka experiment names): ')
+    #print(', '.join(map(lambda x: x.replace('./CT_logs_Mu/', ''), candidate_ckpts)))
+    #print('='*100+'\n'+'='*100+'\n')
+
+    parser = argparse.ArgumentParser(description='Packages/aggregates V2 CT models for ablate.')
+    parser.add_argument('--CPV_source_path', type=str, required=True, help='path to checkpoint *search_directory* of V2 CPV_source model')
+    parser.add_argument('--Souener_path', type=str, required=True, help='path to checkpoint *search_directory* of V2 Souener model')
+    parser.add_argument('--Inverse_path', type=str, required=True, help='path to checkpoint *search_directory* of V2 Inverse model')
     parser.add_argument('--CPV_Weight_matrix_path', type=str, required=True, help='path to the W-matrix csv (the matrix used to compress Yis to CPVs)')
     args = parser.parse_args()
  
     # IMPORTANT: MUST MATCH the 'model_name' variable in adapt_test_targets.py!!
     out_dir = 'PCDNNV2_decomp' # (advised not to change from PCDNNV2_decomp) 
-    os.system('rm -r PCDNNV2_decomp') 
+    os.system('rm -r PCDNNV2_decomp 2> /dev/null') 
     # done without reference to out_dir as a sanity check on adapt_test_targets
 
+    # search for checkpoints inside the search paths
+    ckpt_search_args=['CPV_source_path', 'Souener_path', 'Inverse_path']
+    for arg_name in ckpt_search_args:
+        print(f'searching for {arg_name} checkpoint:') # we test prepending 'CT_logs_Mu/' for brevity
+        try: vars(args)[arg_name]=find_best_ckpt('CT_logs_Mu/'+vars(args)[arg_name])
+        except FileNotFoundError: vars(args)[arg_name]=find_best_ckpt(vars(args)[arg_name])
+    
     aggregate_regressor = make_aggregate_regressor(args.CPV_source_path, args.Souener_path, args.Inverse_path)
     os.system(f'mkdir -p {out_dir}/experiment_records') # for config.yaml files
     aggregate_regressor.save(f'{out_dir}/regressor')
