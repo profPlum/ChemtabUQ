f123b4a7238206c5b3502f1e324eadadc5cfac0d
Dwyer Deighan <dwyerdei@buffalo.com>
2023-10-23 18:00:20 -0400

    commiting what looks like working RESUME fix

--- chemtab_UQ_job_stub.sh
+++ chemtab_UQ_job_stub.sh
@@ -9,45 +9,56 @@ srun nvidia-smi
 
 # --nodes cannot be set dynamically!! (in sheebang)
 # but it can be detected dynammically using srun magic!
-num_nodes=$(srun hostname | sort | uniq -c | wc -l)
 # logic is: prints all hostnames, then sorts, then groups & counts group sizes, then counts number of groups
-
+num_nodes=$(srun hostname | sort | uniq -c | wc -l)
 echo num nodes: $num_nodes
 
 # let the user know if their EXTRA_PL_ARGS went through or not
 [ -z "$EXTRA_PL_ARGS" ] && echo EXTRA_PL_ARGS is empty!! >&2
 # NOTE: EXTRA_PL_ARGS should be an environment variable given by the user when submitting sbatch!!
 
-diagnostic_CLI_args="--trainer.logger=pytorch_lightning.loggers.TensorBoardLogger --trainer.logger.save_dir=. --trainer.logger.name=$SLURM_JOB_NAME --trainer.logger.version=$SLURM_JOB_ID" #--trainer.logger.save_dir=ChemtabUQ_TBlogs"
-diagnostic_CLI_args="$diagnostic_CLI_args --trainer.profiler simple --trainer.callbacks+=pytorch_lightning.callbacks.LearningRateMonitor --trainer.callbacks.logging_interval=epoch --trainer.callbacks+=pytorch_lightning.callbacks.DeviceStatsMonitor --trainer.callbacks+=pytorch_lightning.callbacks.ModelCheckpoint --trainer.callbacks.monitor=val_loss --trainer.callbacks.filename='{epoch}-{val_loss:.4f}-{val_MAPE:.4f}-{val_R2_avg:.4f}'" #--trainer.track_grad_norm 2" 
-lightning_CLI_args="$EXTRA_PL_ARGS $diagnostic_CLI_args --trainer.num_nodes=$num_nodes --trainer.devices=2 --trainer.accelerator=gpu --trainer.strategy=ddp"
-
 # IMPORTANT: gradient_clip defaults set to --trainer.gradient_clip_algorithm=value --trainer.gradient_clip_val=0.5 (inside ChemtabUQ.py)
 # NOTE: gradient_clip_value=0.5 recommended by PL docs
 # NOTE: --max_epochs -1 --> means no max epochs (i.e. fit for entire allocation time)
 # NOTE: --trainer.track_grad_norm 2 is now done automatically inside the model in a way that is compatible with PL v2.*
 
-find_last_ckpt() {
-    last_checkpoint=$(/bin/ls -t $(/bin/find "$1" -name "*.ckpt") | head -n 1)
-    #last_checkpoint=$(/bin/ls -t $1/*/checkpoints/*.ckpt | head -n 1)
-    echo $last_checkpoint
-}
-
-echo RESUME: $RESUME
-if [[ $RESUME == T ]]; then
-    # NOTE: this works for BOTH Sigma & Mean models b/c we use --trainer.default_root_dir which these checkpoint paths will implicitly be relative to!
-    cd CT_logs_Mu
-    last_checkpoint=$(find_last_ckpt ./$SLURM_JOB_NAME/)
-    cd -
-    echo loading last_checkpoint: $last_checkpoint
-    lightning_CLI_args="$lightning_CLI_args --ckpt_path=$last_checkpoint"
-else
-    echo We arent resuming last ckpt... >&2
-fi
+# add diagnostic cli args which should always be set & clear any existing lightning_CLI_args value
+diagnostic_CLI_args="--trainer.logger=pytorch_lightning.loggers.TensorBoardLogger --trainer.logger.save_dir=. --trainer.logger.name=$SLURM_JOB_NAME --trainer.logger.version=$SLURM_JOB_ID" #--trainer.logger.save_dir=ChemtabUQ_TBlogs"
+diagnostic_CLI_args="$diagnostic_CLI_args --trainer.profiler simple --trainer.callbacks+=pytorch_lightning.callbacks.LearningRateMonitor --trainer.callbacks.logging_interval=epoch --trainer.callbacks+=pytorch_lightning.callbacks.DeviceStatsMonitor --trainer.callbacks+=pytorch_lightning.callbacks.ModelCheckpoint --trainer.callbacks.monitor=val_loss --trainer.callbacks.filename='{epoch}-{val_loss:.4f}-{val_MAPE:.4f}-{val_R2_avg:.4f}'" #--trainer.track_grad_norm 2" 
+lightning_CLI_args="--trainer.num_nodes=$num_nodes --trainer.devices=2 --trainer.accelerator=gpu --trainer.strategy=ddp" # NOTE: everything will be combined later, NOT HERE
 
 echo diagnostic_CLI_args: $diagnostic_CLI_args
 echo EXTRA_PL_ARGS: $EXTRA_PL_ARGS
 echo lightning_CLI_args: $lightning_CLI_args
+
+echo RESUME: $RESUME
+if [[ $RESUME == T ]]; then
+    # NOTE: for resume we should exclude $diagnostic_CLI_args & $EXTRA_PL_ARGS!!
+    if [[ "$EXTRA_PL_ARGS" ]]; then
+        echo Error! EXTRA_PL_ARGS must be empty in order to resume properly!! >&2
+        return 2 || exit 2
+    fi
+ 
+    find_last_ckpt() {
+        last_checkpoint=$(/bin/ls -t $(/bin/find "$1" -name "*.ckpt") | head -n 1)
+        echo $last_checkpoint
+    }
+
+    # NOTE: this works for BOTH Sigma & Mean models b/c we use --trainer.default_root_dir which these checkpoint paths will implicitly be relative to!
+    cd CT_logs_Mu
+    echo SLURM_JOB_NAME: $SLURM_JOB_NAME
+    last_checkpoint=$(find_last_ckpt ./$SLURM_JOB_NAME/)
+    last_cfg=$(dirname $last_checkpoint)/../config.yaml
+    cd -
+    echo loading last_checkpoint: $last_checkpoint
+    diagnostic_CLI_args= # we don't want to pass unnecesary arguments
+    lightning_CLI_args="--ckpt_path=$last_checkpoint -c $last_cfg $lightning_CLI_args"
+else
+    echo We arent resuming last ckpt... >&2
+fi
+
+# combine everything
+lightning_CLI_args="$EXTRA_PL_ARGS $lightning_CLI_args $diagnostic_CLI_args"
 
 # setup env based on GPUs allocated
 source /user/dwyerdei/.bash_profile
@@ -69,16 +80,16 @@ mkdir mean_regressors 2> /dev/null
 mv model.ckpt mean_regressors/model-${SLURM_JOB_ID}.ckpt
 cd -
 
+cd CT_logs_Sigma
 if [ $MEAN_ONLY != T ]; then
-    cd CT_logs_Sigma
 	# TODO: fix bad mean regressor loading for cases with multiple jobs running at once
     srun --ntasks-per-node=2 python ../ChemtabUQ.py fit --data.class_path=UQRegressorDataModule --data.mean_regressor_fn=~/ChemtabUQ/CT_logs_Mu/mean_regressors/model-${SLURM_JOB_ID}.ckpt $lightning_CLI_args #--trainer.default_root_dir=CT_logs_Sigma 
 	mkdir UQ_regressors 2> /dev/null
     mv model.ckpt UQ_regressors/model-${SLURM_JOB_ID}.ckpt
-	cd -
 else
 	rm -r UQ_regressor 2> /dev/null
 fi
+cd -
 
 # Example Commands for training of mean_regressor and UQ model
 #srun --ntasks-per-node=2 python ChemtabUQ.py fit --data.class_path=MeanRegressorDataModule --data.data_fn=../data/chrest_contiguous_group_sample100k.csv --trainer.accelerator=gpu --trainer.devices=2 --trainer.num_nodes=2
